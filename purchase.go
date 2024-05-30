f6a5619111e6e227d5aaf5cb8ae7205b2c4bf5bb
rchnmy <85847048+rchnmy@users.noreply.github.com>
2024-05-30 22:15:28 +0300

    fixing indentations

--- purchase.go
+++ purchase.go
@@ -4,32 +4,32 @@ import "fmt"
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	switch kind {
+    switch kind {
         case "car", "truck":
-        	return true
+            return true
     }
     return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	var choice string
-  if option1 < option2 { choice = option1 } else { choice = option2 }
-	return fmt.Sprintf("%s is clearly the better choice.", choice)
+    var choice string
+    if option1 < option2 { choice = option1 } else { choice = option2 }
+        return fmt.Sprintf("%s is clearly the better choice.", choice)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	  // Overengeenering for practice sake
+    // Overengeenering for practice sake
     var discount, actualPrice float64
-	  switch {
-		  case 3 > age:
-			    discount += 20
-		  case (3 <= age) && (age < 10):
-		      discount += 30
-		  case 10 <= age:
-			    discount += 50
+    switch {
+        case 3 > age:
+	    discount += 20
+	case (3 <= age) && (age < 10):
+	    discount += 30
+	case 10 <= age:
+	    discount += 50
     }
-	actualPrice = originalPrice - originalPrice / (100 / discount)
-	return actualPrice
+    actualPrice = originalPrice - originalPrice / (100 / discount)
+    return actualPrice
 }
